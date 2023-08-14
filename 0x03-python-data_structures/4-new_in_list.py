6f6423825c2f7eb547d35a44d509a10c67364cf0
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-08-14 10:18:43 -0700

    fr

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copylist = [x for x in my_list]
+    copylist[idx] = element
+    return (copylist)
+
