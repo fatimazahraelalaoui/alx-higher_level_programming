216f7dfed82033314afe4d7ab4a312ad4eb9692f
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-11-14 10:44:26 -0800

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,7 +4,6 @@ const dKeys = Object.keys(dict);
 const values = Object.values(dict);
 let matched;
 const result = {};
-// loop over the values
 for (let i = 0; i < values.length; i++) {
   result[JSON.stringify(values[i])] = [];
   matched = dKeys.filter(key => dict[key] === values[i]);
